a974e54b61f7b03cc4ae1b1356f8106b9320aee1
Ilya <ilya.popov.sbmt@gmail.com>
2021-06-14 16:44:18 +0200

    Entities improvement

--- src/domain/entities/activity.entity.ts
+++ src/domain/entities/activity.entity.ts
@@ -1,13 +1,21 @@
 import { MoneyEntity } from './money.entity';
 import { AccountId } from './account.entity';
 
+export type ActivityId = number | null;
+
 export class ActivityEntity {
   constructor(
+    private readonly _ownerAccountId: AccountId,
     private readonly _sourceAccountId: AccountId,
     private readonly _targetAccountId: AccountId,
     private readonly _timestamp: Date,
     private readonly _money: MoneyEntity,
+    private readonly _id?: ActivityId,
   ) {}
+
+  get ownerAccountId(): AccountId {
+    return this._ownerAccountId;
+  }
 
   get sourceAccountId(): AccountId {
     return this._sourceAccountId;
